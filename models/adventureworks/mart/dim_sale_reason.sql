690198054cc16dab20390cf586eecc7481833538
pedroribeirophlr@gmail.com <pedroribeirophlr@gmail.com>
2021-11-05 19:43:21 +0000

    commit fact

--- models/adventureworks/mart/dim_sale_reason.sql
+++ models/adventureworks/mart/dim_sale_reason.sql
@@ -4,15 +4,16 @@ with
     staging as (
         select *
         from {{ ref('stg_salesreason') }}
-)
+)   
     , transformed as (
         select
 
             row_number() over (order by salesreasonid) as salesreason_sk -- auto incremental surrogate key
-            , salesreasonid	
+            , salesreasonid
+            , salesorderid 
             , name	
             , reasontype		
-        from staging     
+        from staging
 )
 
 select * from transformed
