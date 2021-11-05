dc703d0097cd0acc56a24879f87f35ebd6e9c592
pedroribeirophlr@gmail.com <pedroribeirophlr@gmail.com>
2021-11-05 20:38:44 +0000

    final commit

--- models/adventureworks/staging/stg_products.sql
+++ models/adventureworks/staging/stg_products.sql
@@ -30,4 +30,14 @@ with
         from {{ source('erpadventureworks20211028','production_product') }}
     )
 
-select * from source_data
+    , source_data_2 as (
+        select
+            salesorderid		
+            , productid		
+    
+        from {{ source('erpadventureworks20211028','sales_salesorderdetail') }}
+    )
+
+select source_data_2.salesorderid, source_data.* 
+from source_data_2 
+left join source_data on source_data_2.productid =  source_data.productid
